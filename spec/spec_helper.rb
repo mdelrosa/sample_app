edb53a152f44f23099347b866b929e219c05d250
Mason <mason.delro@gmail.com>
2013-05-28 17:04:21 -0400

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,9 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  # Add Capybara DSL to RSpec
+  config.include Capybara::DSL
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -39,4 +42,5 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
 end
